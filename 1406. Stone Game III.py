c7c08388ed7c48bb1a9c3594b0e955d500eb90ca
Naman Kumar <namankumar281205@gmail.com>
2026-08-03 23:56:35 +0530

    Create 1406. Stone Game III.py

---
+++ 1406. Stone Game III.py
@@ -0,0 +1,18 @@
+class Solution:
+    def stoneGameIII(self, stoneValue: List[int]) -> str:
+        n = len(stoneValue)
+        dp = [0] * (n + 1)        
+        for i in range(n - 1, -1, -1):
+            best_diff = stoneValue[i] - dp[i + 1]
+            if i + 1 < n:
+                best_diff = max(best_diff, stoneValue[i] + stoneValue[i+1] - dp[i + 2])
+            if i + 2 < n:
+                best_diff = max(best_diff, stoneValue[i] + stoneValue[i+1] + stoneValue[i+2] - dp[i + 3])
+                
+            dp[i] = best_diff
+        if dp[0] > 0:
+            return "Alice"
+        elif dp[0] < 0:
+            return "Bob"
+        else:
+            return "Tie"
