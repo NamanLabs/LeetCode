7b09aeb2eadb9f1dfe125bf1beff0a0442d0e166
Naman Kumar <namankumar281205@gmail.com>
2026-08-27 09:46:16 +0530

    Implement multiple solutions for merge sorted array
    Added three solutions for merging two sorted arrays.

---
+++ 88. Merge Sorted Array.py
@@ -0,0 +1,30 @@
+# SOLUTION 1 :
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        nums1[m:] = nums2
+        nums1.sort()
+# SOLUTION 2 :
+class Solution:
+    def merge(self, nums1: list[int], m: int, nums2: list[int], n: int) -> None:
+        for i in range(n):
+            nums1[m + i] = nums2[i]
+        
+        nums1.sort()
+# SOLUTION 3 :
+class Solution:
+    def merge(self, nums1: list[int], m: int, nums2: list[int], n: int) -> None:
+        p1 = m - 1
+        p2 = n - 1
+        p = m + n - 1
+
+        while p2 >= 0:
+            if p1 >= 0 and nums1[p1] > nums2[p2]:
+                nums1[p] = nums1[p1]
+                p1 -= 1
+            else:
+                nums1[p] = nums2[p2]
+                p2 -= 1
+            p -= 1
