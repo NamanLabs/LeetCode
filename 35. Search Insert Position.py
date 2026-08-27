eae02fb78aad78a32094a546a17214ae3de3a2c1
Naman Kumar <namankumar281205@gmail.com>
2026-08-27 10:01:35 +0530

    Add solutions for search insert position problem
    Implemented two solutions for the search insert position problem using linear and binary search methods.

---
+++ 35. Search Insert Position.py
@@ -0,0 +1,24 @@
+#SOLUTION 1 
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        for i in range(len(nums)):
+            if nums[i] >= target:
+                return i
+        return len(nums)
+
+# SOLUTION 2 
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+        
+        while left <= right:
+            mid = (left + right) // 2
+            
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+                
+        return left
